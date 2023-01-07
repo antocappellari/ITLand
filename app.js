0c99c6769d9c07e8f635dba5e5f0b0bb853fb8a2
JhonatanSoto <sotojhonatan897@gmail.com>
2023-01-07 13:16:58 -0300

    implementacion de node.js y express

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+const PORT = 3000
+
+app.use(express.static(publicPath));
+app.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, './views/index.html')))
+
+
+
+
+
+app.listen(PORT,()=>{console.log(`Servidor corriendo en el puerto ${PORT}`)});
+
+
+
+
+
+
+
+
+
+
+
+
