87cf70f3efbcf9e8f72934c81d3b9fc50f21ade2
JhonatanSoto <sotojhonatan897@gmail.com>
2023-01-17 20:33:32 -0300

    modificando varios archivos parte1

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const publicPath = path.resolve(__dirname, "./public");
-const PORT = 3000;
+const publicPath = path.resolve(__dirname, './public');
+const PORT = process.env.PORT||3000
 
 app.use(express.static(publicPath));
-app.get("/", (req, res) => res.sendFile(path.resolve("./views/index.html")));
-app.get("/login", (req, res) =>
-  res.sendFile(path.resolve("./views/login.html"))
-);
-app.get("/productCart", (req, res) =>
-  res.sendFile(path.resolve("./views/productCart.html"))
-);
-app.get("/productDetail", (req, res) =>
-  res.sendFile(path.resolve("./views/productDetail.html"))
-);
-app.get("/register", (req, res) =>
-  res.sendFile(path.resolve("./views/register.html"))
-);
+app.get('/', (req, res)=> res.sendFile(path.resolve('./views/index.html')))
+app.get('/login',(req,res)=> res.sendFile(path.resolve('./views/login.html')))
+app.get('/productCart', (req, res) => res.sendFile(path.resolve('./views/productCart.html')))
+app.get('/productDetail', (req, res) => res.sendFile(path.resolve('./views/productDetail.html')))
+app.get('/register',(req,res)=> res.sendFile(path.resolve('./views/register.html')))
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+
+
+
+app.listen(PORT,()=>{console.log(`Servidor corriendo en el puerto ${PORT}`)});
+
+
+
+
+
+
+
+
+
+
+
+
