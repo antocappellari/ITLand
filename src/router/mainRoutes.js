3a4ba9baed40f0ade90ba0e969ce417fc05e46a3
antocappellari <antonella.cappellari@outlook.com>
2023-02-21 15:24:09 -0300

    rutas main p<ra detail

--- src/router/mainRoutes.js
+++ src/router/mainRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController')
+const productsController = require('../controllers/productsController')
 
 //--------------- Rutas-----------------//
 router.get('/', mainController.index)
+router.get('/detail/:id?', productsController.productDetail)
 
 // exportacion de rutas
 module.exports = router
