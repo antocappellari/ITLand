16def0718df852e8e6232279ed1e623456984cc2
antocappellari <antonella.cappellari@outlook.com>
2023-05-01 19:48:22 -0300

    front

---
+++ public/JS/js-front-user.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",function(){
+    let formulario = document.querySelector(".form-inputs")
+    this.alert("vinculado")
+    formulario.addEventListener("submit",function(e){
+        let errores = [];
+        let nameField =
+    })
+})
