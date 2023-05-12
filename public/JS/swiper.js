3437f0b549ec4ad2344b913e6bd71329141101cf
JhonatanSoto <sotojhonatan897@gmail.com>
2023-05-11 21:38:18 -0300

    agregando swiper en index.ejs

--- public/JS/swiper.js
+++ public/JS/swiper.js
@@ -1,25 +1,90 @@
-alert("hello world")
-console.log('hola');
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
+    direction: 'horizontal',
+    loop: false,
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      670: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      }
+    },
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
   });
+    const swiper2 = new Swiper('.swiper2', {
+        autoplay: {
+            delay: 5000,
+          },
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+     
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      }
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+  });
+  const swiper3 = new Swiper('.swiper3', {
+    autoplay: {
+        delay: 5000,
+      },
+// Optional parameters
+direction: 'horizontal',
+loop: true,
+
+// If we need pagination
+pagination: {
+  el: '.swiper-pagination',
+},
+breakpoints: {
+ 
+  480: {
+    slidesPerView: 1,
+    spaceBetween: 20
+  }
+},
+
+// Navigation arrows
+// navigation: {
+//   nextEl: '.swiper-button-next',
+//   prevEl: '.swiper-button-prev',
+// },
+
+// And if we need scrollbar
+});
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,6 @@
 const ProductServices = require('../services/ProductServices');
 const mainController = {
     index :async (req,res) => {
-
-let c = await ProductServices.getAllProducts();
-console.log(c[0].images);
         try {
             res.render('index.ejs',{
                 products: await ProductServices.getAllProducts()
